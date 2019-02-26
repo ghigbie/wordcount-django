fe1724f27f27fe0e12f97c496e5fc3c466da4133
George Higbie <ghigbie@monigle.com>
2019-02-26 08:37:00 -0700

    Add new template and path

--- wordcount/views.py
+++ wordcount/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def homepage(request):
     return HttpResponse(request, 'home.html', {'title': 'Django Word Count'})
+
+def countpage(request):
+    return HttpResponse(request, 'count.html', {'title': 'Count Page'})
