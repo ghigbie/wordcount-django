43eab01907ce92e237ee2cfc9f6b40a200601b99
George Higbie <ghigbie@monigle.com>
2019-02-28 10:39:00 -0700

    Add new home routing

--- wordcount/views.py
+++ wordcount/views.py
@@ -19,3 +19,6 @@ def count(request):
 
     sortedwords = sorted(worddictionary.items(), key=operator.itemgetter(1), reverse=True)
     return render(request, 'count.html', {'fulltext': fulltext, 'length': len(wordlist), 'sortedwords': sortedwords })
+
+def about(request):
+    return render(request, 'about.html')
