403037c5307fc2828c05d634b8aa38a440ae9679
George Higbie <ghigbie@monigle.com>
2019-02-28 09:39:19 -0700

    Lowercased words

--- wordcount/views.py
+++ wordcount/views.py
@@ -12,9 +12,10 @@ def count(request):
     worddictionary = {}
     for word in wordlist:
         if word in worddictionary:
+            word = word.lower()
             worddictionary[word] += 1
         else:
             worddictionary[word] = 1
 
     sortedwords = sorted(worddictionary.items(), key=operator.itemgetter(1), reverse=True)
-    return render(request, 'count.html', {'fulltext': fulltext, 'length': len(wordlist), 'sortedWords': sortedwords })
+    return render(request, 'count.html', {'fulltext': fulltext, 'length': len(wordlist), 'sortedwords': sortedwords })
